a15e92bc0ed634669c5df06187eef9fe0955f916
Shubham S Gurung <gurungshubham65@gmail.com>
2024-07-11 18:55:50 -0400

    added aboutHandler function

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the Home Page!")
 }
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is a simple API server built with Go.")
+}
 func main() {
 
 }
